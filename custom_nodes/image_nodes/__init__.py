edc266117d435bf9d48b0daa4594c854367f7a0d
Yudhisteer <yudhisteer.chin@gmail.com>
2024-06-09 02:05:47 -0700

    add custom nodes with easynodes

---
+++ custom_nodes/image_nodes/__init__.py
@@ -0,0 +1,12 @@
+# Simply importing your module gives it a chance to add the @ComfyFunc nodes since
+# EasyNodes will automatically export the NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS
+# for you.
+from .process_image_nodes import *
+
+
+# Alternatively, to export yourself, you can do the following:
+# import easy_nodes
+# easy_nodes.init(auto_register=False)
+# import example.example_nodes
+# NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = easy_nodes.get_node_mappings()
+# __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
